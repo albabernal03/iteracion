b437e6aafb2bba7da6a5fb7abb9ed856eb72bf9d
albabernal03 <ALBABERO@MYUAX.COM>
2022-02-23 17:20:19 +0100

    pruebas

---
+++ ejercicio_10.py
@@ -0,0 +1,13 @@
+nombre = str(input('Introduzca su nombre y apellido:'))
+edad = int(input('Introduzca su edad:'))
+padres = int(input('Selecione (1) si tiene padres, (2) si solo madre o padre y (3) si no tiene:'))
+
+class información:
+    def __init__(self,nombre,edad,padres):
+        self.nombre= nombre
+        self.edad = edad 
+        self.padres = padres 
+
+    def comparación_edad (self):
+        if 20 < self.edad < 30: 
+            print(nombre)
