5808bc467bc63c8505f852db09b0f49f27e1b467
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-02-23 17:46:49 +0000

    he pegado lo que tenia en studio visual code

---
+++ ejercicio_6.py
@@ -0,0 +1,29 @@
+palabras = []
+palabra1 = str(input('Introduzca una palabra que contenga a:'))
+palabras.append(palabra1)
+palabra2 = str(input('Introduzca ota palabra que contenga a:'))
+palabras.append(palabra2)
+palabra3 = str(input('Introduzca la última palabra que contenga a:'))
+palabras.append(palabra3)
+
+
+class palabra:
+    def __init__(self,palabra1, palabra2, palabra3):
+        self.palabra1 = palabra1
+        self.palabra2 = palabra2
+        self.palabra3 = palabra3
+
+    def palabras_con_a(self):
+        if palabra1.startswith('a') or palabra1.startswith('A') and palabra2.startswith('a') or palabra2.startswith('A') and palabra3.startswith('a')or palabra3.startswith('A'):
+            print(palabra1, palabra2,palabra3)
+        else:
+            pass
+    
+    def ordenar_alfabéticamente (self):
+        palabras.sort()
+        print(palabras)
+        
+
+       
+
+
